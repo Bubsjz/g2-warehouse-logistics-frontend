23fcdfea2b09e19867427ed533322ded309de00d
Rodrigo <rofraiesjob@gmail.com>
2024-10-22 22:25:08 +0200

    Cuerpo listo a falta de mejoras de diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
