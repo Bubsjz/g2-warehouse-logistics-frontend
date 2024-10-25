8f3a104403038cf92b8f32ae25baf7773127e7f6
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-10-25 14:17:49 +0200

    warehouseTable  incluido en componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { WarehouseTableComponent } from "./components/warehouse-table/warehouse-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, WarehouseTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
