c430767eda004cbb56fbab473b008a7a5ace9b1b
Laura <laugain8@gmail.com>
2024-10-27 19:18:26 +0100

    Importación componente orderForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { OrderFormComponent } from './components/order-form/order-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, OrderFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
