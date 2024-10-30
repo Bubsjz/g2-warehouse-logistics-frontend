fda22eceeb5cd79f557d72ee581378075f9ef647
Lola <lola_chacon8@hotmail.com>
2024-10-30 22:24:58 +0100

    rutas y router-outlet en dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'', pathMatch:'full', redirectTo:'login'},
+    {path:'login', component:LoginComponent},
+    {path:'dashboard', component:DashboardComponent,children:[
+       
+
+    ]},
+    {path:'**', redirectTo:'login'}
+];
