c33d9dc8e722629aa83d938d33cffefad3b5767e
Berard <berard_65@hotmail.com>
2024-10-31 19:02:54 +0100

    Ajuste merge login_v2 a routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 
 
-=======
->>>>>>> feature_paginator
 @Component({
   selector: 'app-root',
   standalone: true,
