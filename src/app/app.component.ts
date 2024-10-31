b4fbca1e985883505fb218334940e87efd60ce11
fran <fran10.jimenezromera@gmail.com>
2024-10-31 18:38:19 +0100

    Eliminación de LoginComponent
    Se ha dejado únicamente el Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, PaginatorComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
