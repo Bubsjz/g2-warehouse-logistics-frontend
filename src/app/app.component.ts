35fd06264dce334105a0b2e852153143a1f20f14
fran <fran10.jimenezromera@gmail.com>
2024-10-31 18:32:23 +0100

    Componente Paginator Acabado
    Introduces el numero de paginas y devuelve la pagina donde estas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-
+import { PaginatorComponent } from './components/paginator/paginator.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, PaginatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'g2-warehouse-logistics-frontend';
+
+  ngOnInit(): void {
+    this.sendPage();
+      }
+  sendPage(currentPage:string = "1"){
+    console.log(currentPage);
+  }
+
 }
