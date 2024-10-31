513d49fc300ca5bb8c73f4dfacf9c790e3046ccd
Berard <berard_65@hotmail.com>
2024-10-31 23:41:05 +0100

    Ajuste merge feature_login_v2 into routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { OrderTableComponent } from "./components/order-table/order-table.component";
-
 import { LoginComponent } from './components/login/login.component';
+import { OrderTableComponent } from "./components/order-table/order-table.component";
 import { OrderFormComponent } from './components/order-form/order-form.component';
 import { WarehouseFormComponent } from './components/warehouse-form/warehouse-form.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { WarehouseViewComponent } from './pages/warehouse-view/warehouse-view.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
 import { MainFooterComponent } from './components/main-footer/main-footer.component';
-=======
 import { NavbarComponent } from './components/navbar/navbar.component';
->>>>>>> feature_barnav
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, OrderTableComponent],
-  imports: [RouterOutlet, LoginComponent, OrderFormComponent],
-  imports: [RouterOutlet, LoginComponent, WarehouseFormComponent],
-  imports: [RouterOutlet, UserFormComponent],
-  imports: [RouterOutlet, LoginComponent, WarehouseViewComponent],
-  imports: [RouterOutlet, MainHeaderComponent],
-  imports: [RouterOutlet, MainFooterComponent],
-=======
-  imports: [RouterOutlet, NavbarComponent],
->>>>>>> feature_barnav
+  imports: [RouterOutlet,OrderTableComponent, LoginComponent, OrderFormComponent, WarehouseFormComponent, UserFormComponent, WarehouseViewComponent, MainHeaderComponent, MainFooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
