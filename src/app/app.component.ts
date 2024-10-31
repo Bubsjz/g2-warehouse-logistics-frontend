e3ccb2e0f2149d2552e2634f58df8d967886eb20
Rodrigo <rofraiesjob@gmail.com>
2024-10-31 19:30:24 +0100

    Ajuste merge feature footer into routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { OrderTableComponent } from "./components/order-table/order-table.component";
 
 import { LoginComponent } from './components/login/login.component';
@@ -9,23 +8,18 @@ import { WarehouseFormComponent } from './components/warehouse-form/warehouse-fo
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { WarehouseViewComponent } from './pages/warehouse-view/warehouse-view.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
-=======
 import { MainFooterComponent } from './components/main-footer/main-footer.component';
->>>>>>> feature_footer
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [RouterOutlet, OrderTableComponent],
   imports: [RouterOutlet, LoginComponent, OrderFormComponent],
   imports: [RouterOutlet, LoginComponent, WarehouseFormComponent],
   imports: [RouterOutlet, UserFormComponent],
   imports: [RouterOutlet, LoginComponent, WarehouseViewComponent],
   imports: [RouterOutlet, MainHeaderComponent],
-=======
   imports: [RouterOutlet, MainFooterComponent],
->>>>>>> feature_footer
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
