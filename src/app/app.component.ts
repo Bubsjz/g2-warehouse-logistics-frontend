e509897e31d91c5bd4b32832337b5a1c688ea970
Lola <lola_chacon8@hotmail.com>
2024-11-01 17:23:44 +0100

    Quito enlaces de todos los componentes al component.htlm
    importo router-outler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { OrderTableComponent } from "./components/order-table/order-table.component";
-import { OrderFormComponent } from './components/order-form/order-form.component';
-import { WarehouseFormComponent } from './components/warehouse-form/warehouse-form.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
-import { WarehouseViewComponent } from './pages/warehouse-view/warehouse-view.component';
-import { MainHeaderComponent } from './components/main-header/main-header.component';
-import { MainFooterComponent } from './components/main-footer/main-footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,OrderTableComponent, LoginComponent, OrderFormComponent, WarehouseFormComponent, UserFormComponent, WarehouseViewComponent, MainHeaderComponent, MainFooterComponent, NavbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
