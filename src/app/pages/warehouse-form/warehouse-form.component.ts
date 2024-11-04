d47476441a371c80e201cb55bad470ced3ac66dd
Laura <laugain8@gmail.com>
2024-11-04 14:05:10 +0100

    warehouseForm importación ReactiveFormsModule

--- src/app/pages/warehouse-form/warehouse-form.component.ts
+++ src/app/pages/warehouse-form/warehouse-form.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-warehouse-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './warehouse-form.component.html',
   styleUrl: './warehouse-form.component.css'
 })
 export class WarehouseFormComponent {
+  warehouseForm: FormGroup
 
+  constructor(){
+    this.warehouseForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      locality: new FormControl(null, [Validators.required]),
+      address: new FormControl(null, [Validators.required]),
+      image: new FormControl(null, [])
+    })
+  }
+
+  getDataForm(){
+    if (this.warehouseForm.valid){
+      console.log('Formulario válido: '), this.warehouseForm.value
+    } else {
+      console.log('Formulario no válido')
+      this.warehouseForm.markAllAsTouched()
+    }
+  }
 }
