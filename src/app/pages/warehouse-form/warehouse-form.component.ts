569c03ae0b37a1c51f930990752da8390d1469d7
Laura <laugain8@gmail.com>
2024-11-04 16:01:40 +0100

    warehouseForm validaciones TS

--- src/app/pages/warehouse-form/warehouse-form.component.ts
+++ src/app/pages/warehouse-form/warehouse-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-warehouse-form',
@@ -9,23 +9,52 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './warehouse-form.component.css'
 })
 export class WarehouseFormComponent {
-  warehouseForm: FormGroup
+  warehouseForm: FormGroup;
+  selectedImage: File | null = null
 
   constructor(){
     this.warehouseForm = new FormGroup({
       name: new FormControl(null, [Validators.required]),
       locality: new FormControl(null, [Validators.required]),
       address: new FormControl(null, [Validators.required]),
-      image: new FormControl(null, [])
+      image: new FormControl(null, [Validators.required, this.imageUrlValidator])
     })
   }
 
-  getDataForm(){
-    if (this.warehouseForm.valid){
-      console.log('Formulario válido: '), this.warehouseForm.value
-    } else {
-      console.log('Formulario no válido')
-      this.warehouseForm.markAllAsTouched()
+  imageUrlValidator = (control: AbstractControl): {[key: string]: any} | null => {
+    const file = control.value
+    if (!file) return null
+
+    const validExtensions = ['jpeg', 'jpg', 'gif', 'png']
+    const extension = file.name.split('.').pop().toLowerCase()
+    return validExtensions.includes(extension) ? null : {invalidImageFile: true}
+  }
+
+  onFileSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0){
+      const file = input.files[0]
+      this.selectedImage = file;
+      this.warehouseForm.patchValue({ image: file})
     }
+  }
+
+
+  getDataForm(){
+    if (this.warehouseForm.invalid){
+      alert('Por favor, completa correctamente todos los campos antes de enviar.')
+      return
+    }
+    const formData = new FormData()
+
+    formData.append('name', this.warehouseForm.get('name')?.value)
+    formData.append('locality', this.warehouseForm.get('locality')?.value)
+    formData.append('address', this.warehouseForm.get('address')?.value)
+    if (this.selectedImage){
+      formData.append('image', this.selectedImage)
+    }
+    alert('Formulario enviado correctamente')
+    this.warehouseForm.reset()
+    this.selectedImage = null
   }
 }
