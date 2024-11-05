a93ed4a718329c90812443107ffdd2e9533a0fee
Rodrigo <rofraiesjob@gmail.com>
2024-11-05 11:06:42 +0100

    Control de aparición en páginas menos /login y ajustes de diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { WarehouseCardComponent } from "./pages/warehouse-card/warehouse-card.component";
 import { WarehouseUserListComponent } from "./pages/warehouse-user-list/warehouse-user-list.component";
 import { WarehouseViewComponent } from "./pages/warehouse-view/warehouse-view.component";
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WarehouseCardComponent, WarehouseUserListComponent, WarehouseViewComponent],
+  imports: [RouterOutlet, CommonModule, WarehouseCardComponent, WarehouseUserListComponent, WarehouseViewComponent, MainHeaderComponent, MainFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'g2-warehouse-logistics-frontend';
+
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
     this.sendPage();
