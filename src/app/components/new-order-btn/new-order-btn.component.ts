976a4034824323c48ff9d4a41538180b9fd87b82
Lola <lola_chacon8@hotmail.com>
2024-11-17 14:32:14 +0100

    importacion RouterLink en newOrder

--- src/app/components/new-order-btn/new-order-btn.component.ts
+++ src/app/components/new-order-btn/new-order-btn.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-order-btn',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-order-btn.component.html',
   styleUrl: './new-order-btn.component.css'
 })
