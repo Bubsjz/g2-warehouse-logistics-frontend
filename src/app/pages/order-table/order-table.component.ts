2a83e9a844cad2e9140113b77006ac4df85d2bcf
Lola <lola_chacon8@hotmail.com>
2024-11-17 13:37:13 +0100

    modificaciones de order-table y ediciones en css

--- src/app/pages/order-table/order-table.component.ts
+++ src/app/pages/order-table/order-table.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { PaginatorComponent } from '../../components/paginator/paginator.component';
+import { Component, inject } from "@angular/core";
+import { NewOrderBtnComponent } from "../../components/new-order-btn/new-order-btn.component";
+import { OrderFilterComponent } from "../../components/order-filter/order-filter.component";
+import { PaginatorComponent } from "../../components/paginator/paginator.component";
+import { Deliveri } from "../../interfaces/deliveri.interface";
+import { DeliveriService } from "../../services/deliveri.service";
+
+
 
 @Component({
   selector: 'app-order-table',
   standalone: true,
-  imports: [PaginatorComponent],
+  imports: [PaginatorComponent, NewOrderBtnComponent, OrderFilterComponent],
   templateUrl: './order-table.component.html',
   styleUrl: './order-table.component.css'
 })
 export class OrderTableComponent {
+
+  deliveriService = inject(DeliveriService);
+  arrDeliveri: Deliveri[]= []
+
+
+  async ngOnInit() {
+    
+    this.arrDeliveri = await this.deliveriService.getAll();
+  }
+
+
+  filterPadre(event: Deliveri[]) {
+    this.arrDeliveri = event;
+  }
 
 }
