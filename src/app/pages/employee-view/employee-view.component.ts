8a7b0fdab60a77fc9e987f95173e6bf09c5278c0
Rodrigo <rofraiesjob@gmail.com>
2024-11-18 20:03:41 +0100

    Preparadas las conexiones y ajustes de estilos. Opción Delete a revisar
    El Delete se supone tendría que funcionar, pero json-server puede no estar tomándolo bien, por lo que a revisar.

--- src/app/pages/employee-view/employee-view.component.ts
+++ src/app/pages/employee-view/employee-view.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { iUser } from '../../interfaces/user.interface';
+import { iWarehouse } from '../../interfaces/warehouse.interface';
 
 @Component({
   selector: 'app-employee-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-view.component.html',
   styleUrl: './employee-view.component.css'
 })
-export class EmployeeViewComponent {
+
+export class EmployeeViewComponent implements OnInit {
+  userInfo: iUser | null = null;
+  warehouseInfo: iWarehouse | null = null;
+
+  constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    const userId = Number(this.route.snapshot.paramMap.get('id'));
+    this.userService.getUserWithWarehouse(userId).subscribe({
+      next: ({ user, warehouse }) => {
+        this.userInfo = user;
+        this.warehouseInfo = warehouse;
+      },
+      error: (err) => {
+        console.error('Error fetching data:', err);
+      },
+    });
+  }
+
+  deleteUser(): void {
+    if (this.userInfo) {
+      const confirmed = confirm(`¿Are you sure you want to remove ${this.userInfo.name} ${this.userInfo.surname}?`);
+  
+      if (confirmed) {
+        this.userService.deleteUser(this.userInfo.id_user).subscribe({
+          next: () => {
+            alert('User deleted successfully.');
+            this.router.navigate(['/dashboard/boss/employee-view']);
+          },
+          error: (err) => {
+            console.error('Error deleting user:', err);
+            alert('An error occurred while deleting the user.');
+          },
+        });
+      }
+    } else {
+      alert('No user information was found for deletion.');
+    }
+  }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, switchMap } from 'rxjs';
+import { iUser } from '../interfaces/user.interface';
+import { iWarehouse } from '../interfaces/warehouse.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private userApiUrl = 'http://localhost:3000/users';
+  private warehouseApiUrl = 'http://localhost:3000/warehouses';
+
+  constructor(private http: HttpClient) {}
+
+    // Obtener usuario con almacén asociado
+    getUserWithWarehouse(id_user: number): Observable<{ user: iUser; warehouse: iWarehouse }> {
+      return this.http.get<iUser[]>(`${this.userApiUrl}?id_user=${id_user}`).pipe(
+        switchMap((users) => {
+          if (users.length === 0) {
+            throw new Error('User not found');
+          }
+          const user = users[0];
+          return this.http.get<iWarehouse[]>(`${this.warehouseApiUrl}?id_warehouse=${user.asigned_id_warehouse}`).pipe(
+            map((warehouses) => {
+              if (warehouses.length === 0) {
+                throw new Error('Warehouse not found');
+              }
+              return { user, warehouse: warehouses[0] };
+            })
+          );
+        })
+      );
+    }
+
+    deleteUser(id_user: number): Observable<void> {
+      const url = `http://localhost:3000/users/${id_user}`;
+      return this.http.delete<void>(url);
+    }
+
+  }
+
