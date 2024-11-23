6523574531dca7fd50e3bb82f2a185d78c8a44e1
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-11-23 01:33:00 +0100

    interfaces y servicios para usuarios y almacenes y datos en carpeta db

---
+++ src/app/interfaces/iuser.interface.ts
@@ -0,0 +1,9 @@
+export interface Iuser {
+    id?: number;
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+    rol: string;
+    warehouse_id?: number;
+}
---
+++ src/app/services/warehouses.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Iwarehouse } from '../interfaces/iwarehouse.interface';
+import { WAREHOUSES } from '../db/warehouses.db';
+import { Iuser } from '../interfaces/iuser.interface';
+import { USERS } from '../db/users.db';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WarehousesService {
+
+  private arrWarehouses: Iwarehouse[] = WAREHOUSES;
+  private arrUsers: Iuser[] = USERS;
+
+  getAll(): Iwarehouse[] {
+    return this.arrWarehouses;
+  }
+}
