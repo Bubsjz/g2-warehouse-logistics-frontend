8da94ad3bb1a0257d7e99c84c996e382265f4419
Lola <lola_chacon8@hotmail.com>
2024-11-26 18:30:45 +0100

    cambios en userService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,10 @@ import { Observable, map, switchMap } from 'rxjs';
 import { iUser } from '../interfaces/user-view.interface';
 import { iWarehouse } from '../interfaces/warehouse-user-view.interface';
 
+import { inject} from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { User } from '../interfaces/user.interface';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -12,6 +16,8 @@ export class UserService {
   private warehouseApiUrl = 'http://localhost:3000/warehouses';
 
   constructor(private http: HttpClient) {}
+
+  // VISTA USUARIO
 
     // Obtener usuario con almacén asociado
     getUserWithWarehouse(id_user: number): Observable<{ user: iUser; warehouse: iWarehouse }> {
@@ -38,34 +44,33 @@ export class UserService {
       return this.http.delete<void>(url);
     }
 
+    // parte de Lola(ORDER-TABLE)
+
+    private baseUrl: string = "http://localhost:3001/user"
+    private httpClient = inject(HttpClient);
+  
+  
+  
+    getAll(){
+     return lastValueFrom(this.httpClient.get<User[]>(this.baseUrl));
+    }
+  
+    getById(id_user:number){
+      return lastValueFrom(this.httpClient.get<User[]>(`${this.baseUrl}?id_user=${id_user}`))
+    }
+  
+
   }
 
 
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
-import { User } from '../interfaces/user.interface';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
-
-  private baseUrl: string = "http://localhost:3001/user"
-  private httpClient = inject(HttpClient);
 
 
 
-  getAll(){
-   return lastValueFrom(this.httpClient.get<User[]>(this.baseUrl));
-  }
 
-  getById(id_user:number){
-    return lastValueFrom(this.httpClient.get<User[]>(`${this.baseUrl}?id_user=${id_user}`))
-  }
 
+ 
  
 
  
 
-}
+
