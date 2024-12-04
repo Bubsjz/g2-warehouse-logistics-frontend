030ea5c0ed76bb23defa043dfbcfbc4b6c6cd77a
Lola <lola_chacon8@hotmail.com>
2024-12-04 18:08:55 +0100

    modificaciones post-merge

--- src/app/services/warehouses.service.ts
+++ src/app/services/warehouses.service.ts
@@ -2,7 +2,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Iwarehouse } from '../interfaces/iwarehouse.interface';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { lastValueFrom } from 'rxjs';
 import { Iwarehouse2 } from '../interfaces/iwarehouse2.interface';
 
@@ -19,37 +18,18 @@ image: File,
 export class WarehousesService {
 
   private baseUrl: string = "http://localhost:3000/boss";
-  private httpClient = inject(HttpClient)
-  private baseUrl: string = "http://localhost:3000/warehouses";
-  private Url: string = "http://localhost:3000/boss"
   private httpClient = inject(HttpClient);
 
   getAll(): Promise<Iwarehouse[]> {
     return lastValueFrom(this.httpClient.get<Iwarehouse[]>(`${this.baseUrl}/warehouse`))
    }
-    return lastValueFrom(this.httpClient.get<Iwarehouse[]>(this.baseUrl));
-  }
-
-  getById(id: number): Promise<Iwarehouse[]> {
-    return lastValueFrom(this.httpClient.get<Iwarehouse[]>(`${this.baseUrl}?id=${id}`));
-  }
 
   getById(id: number): Promise<Iwarehouse2> {
     return lastValueFrom(this.httpClient.get<Iwarehouse2>(`${this.baseUrl}/warehouse/${id}`))
   }
-  // Function to save or update a warehouse
-  saveWarehouse(warehouse: any): Observable<any> {
-    if (warehouse.id) {
-      // Update existing warehouse
-      return this.httpClient.put(`${this.baseUrl}/${warehouse.id}`, warehouse);
-    } else {
-      // Create new warehouse
-      return this.httpClient.post(this.baseUrl, warehouse);
-    }
-  }
 
   createWarehouse(body:FormData){
-    return lastValueFrom(this.httpClient.post<warehouse>(`${this.Url}/warehouse`, body))
+    return lastValueFrom(this.httpClient.post<warehouse>(`${this.baseUrl}/warehouse`, body))
   }
 
 }
